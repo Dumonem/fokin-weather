5ea44c26868779b68fdcfd609ec25ed6431d6483
changuk <changuk@mediacore.kr>
2021-05-17 17:56:19 +0900

    노마드코드 리액트 네이티브

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>텍스트 테스트</Text>
       <StatusBar style="auto" />
     </View>
   );
